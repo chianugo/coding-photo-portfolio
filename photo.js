1adb5a121b4c81d1a97ceeed327e55e2ac2986e2
cko003 <cko003@bucknell.edu>
2023-08-23 17:03:16 -0400

    work on picture filtering

--- photo.js
+++ photo.js
@@ -25,3 +25,52 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
+
+filterSelection("kiz"); // Begin by showing fete
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("slide");
+  // if (c == "all") c = "";
+  // hide elemetns that are not active
+  for (i = 0; i < x.length; i++) {
+    console.log(x.length)
+    AddClass(x[i], "hidden");
+    if (x[i].className.indexOf(c) > -1) RemoveClass(x[i], "hidden");
+  }
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (underline it)
+var btnContainer = document.getElementById("side");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
