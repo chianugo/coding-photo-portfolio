7bb183b5b6c4213ce34707a5fb12f22411d08923
cko003 <cko003@bucknell.edu>
2024-02-18 23:57:15 +0100

    change color format

--- script.js
+++ script.js
@@ -69,27 +69,27 @@ window.addEventListener("scroll", () => {
 
 function getRandomColor() {
   const colors = [
-    "#FF0000",
+    "hsl(360deg 100% 50%)",
     "#FF7F00",
     "#00FF00",
-    "#0000FF",
-    "#4B0082",
+    "hsl(240deg 100% 50%)",
+    "hsl(275deg 100% 25%)",
     "#9400D3",
     "#FF1493",
     "#00CED1",
-    "#FF4500",
+    "hsl(16deg 100% 50%)",
     "#9932CC",
     "#800080",
     "#FF69B4",
     "#1E90FF",
-    "#FFD700",
+    "hsl(51deg 100% 50%)",
     "#32CD32",
     "#FF6347",
     "#6A5ACD",
-    "#FF00FF",
+    "hsl(300deg 100% 50%)",
     "#DC143C",
     "#8A2BE2",
-    "#000000",
+    "hsl(0deg 0% 0%)",
     "#FFFF00",
     "#FFC0CB",
     "#A52A2A",
@@ -98,32 +98,32 @@ function getRandomColor() {
     "#323D25",
     "#200f8e",
     "#0ff786",
-    "#260f58",
-    "#061e07",
-    "#dc2c45",
-    "#5a0610",
-    "#0676cb",
-    "#f26bc3",
-    "#0ac0fe",
-    "#df2c8c",
-    "#fafb1a",
-    "#34b70c",
-    "#dc6f8c",
-    "#e3eb1c",
-    "#070703",
-    "#bbe70e",
-    "#ef77eb",
-    "#b5d16f",
-    "#ecba32",
-    "#4b0032",
-    "#f9805a",
-    "#001c5d",
-    "#04121b",
-    "#05011a",
-    "#1c3929",
-    "#031e04",
-    "#b10b6b",
-    "#256288",
+    "hsl(259deg 71% 20%)",
+    "hsl(122deg 67% 7%)",
+    "hsl(351deg 72% 52%)",
+    "hsl(353deg 87% 19%)",
+    "hsl(206deg 94% 41%)",
+    "hsl(321deg 84% 68%)",
+    "hsl(195deg 99% 52%)",
+    "hsl(328deg 74% 52%)",
+    "hsl(60deg 97% 54%)",
+    "hsl(106deg 88% 38%)",
+    "hsl(344deg 61% 65%)",
+    "hsl(62deg 84% 52%)",
+    "#hsl(60deg 40% 2%)",
+    "hsl(72deg 89% 48%)",
+    "hsl(302deg 79% 70%)",
+    "hsl(77deg 52% 63%)",
+    "hsl(44deg 83% 56%)",
+    "hsl(320deg 100% 15%)",
+    "hsl(14deg 93% 66%)hsl(14deg 93% 66%)",
+    "hsl(222deg 100% 18%)",
+    "hsl(203deg 74% 6%)",
+    "hsl(250deg 93% 5%)",
+    "hsl(147deg 34% 17%)",
+    "hsl(122deg 82% 6%)",
+    "hsl(325deg 88% 37%)",
+    "hsl(203deg 57% 34%)",
   ];
 
   for (let i = colors.length - 1; i > 0; i--) {
@@ -161,25 +161,25 @@ function applyRandomColor() {
 
 function toggleMonochrome() {
   var probability = Math.random() < 0.5;
-  if (mainColor === "#000000") {
-    mainColor = "#ffffff";
-    accentColor = "#000000";
+  if (mainColor === "hsl(0deg 0% 0%)") {
+    mainColor = "hsl(0deg 0% 100%)";
+    accentColor = "hsl(0deg 0% 0%)";
   } else {
-    mainColor = "#000000";
-    accentColor = "#ffffff";
+    mainColor = "hsl(0deg 0% 0%)";
+    accentColor = "hsl(0deg 0% 100%)";
   }
-  // else if (mainColor === 'ffffff'){
-  //     mainColor = '#000000';
-  //     accentColor = '#ffffff';
+  // else if (mainColor === hsl(0deg 0% 100%)'){
+  //     mainColor = 'hsl(0deg 0% 0%)';
+  //     accentColor = 'hsl(0deg 0% 100%)';
   // }
   // Split into four conditions because I only want the 50/50 chance if coming from color, else, it should just cycle
   // else if (probability < 0.5) {
-  //     mainColor = '#ffffff';
-  //     accentColor = '#000000';
+  //     mainColor = 'hsl(0deg 0% 100%)';
+  //     accentColor = 'hsl(0deg 0% 0%)';
   // }
   // else if (probability >= 0.5){
-  //     mainColor = '#000000';
-  //     accentColor = '#ffffff';
+  //     mainColor = 'hsl(0deg 0% 0%)';
+  //     accentColor = 'hsl(0deg 0% 100%)';
   document.documentElement.style.setProperty("--main-color", mainColor);
   document.documentElement.style.setProperty("--accent-color", accentColor);
   localStorage.setItem("mainColor", mainColor);
@@ -195,53 +195,56 @@ function toggleMonochrome() {
 function applyColorCombination() {
   // Define color combinations
   const colorCombinations = [
-    { main: "#FF0000", accent: "#000000" },
-    { main: "#000000", accent: "#FFFF00" },
-    { main: "#008000", accent: "#FFC0CB" },
-    { main: "#FFFFFF", accent: "#A52A2A" },
-    { main: "#00539C", accent: "#EEA47F" },
-    { main: "#E0FC5F", accent: "#8638CD" },
-    { main: "#F44962", accent: "#0F050C" },
-    { main: "#c8db79", accent: "#323D25" },
-    { main: "#ff69b4", accent: "#200f8e" },
-    { main: "#0000ff", accent: "#0ff786" },
-    { main: "#ff4500", accent: "#260f58" },
-    { main: "#ff4500", accent: "#07011a" },
-    { main: "#ff4500", accent: "#061e07" },
-    { main: "#ffffff", accent: "#dc2c45" },
-    { main: "#ffffff", accent: "#5a0610" },
-    { main: "#ffffff", accent: "#0676cb" },
-    { main: "#000000", accent: "#f26bc3" },
-    { main: "#000000", accent: "#0ac0fe" },
-    { main: "#000000", accent: "#df2c8c" },
-    { main: "#000000", accent: "#fafb1a" },
-    { main: "#000000", accent: "#34b70c" },
-    { main: "#000000", accent: "#dc6f8c" },
-    { main: "#565a53", accent: "#e3eb1c" },
-    { main: "#007fb8", accent: "#070703" },
-    { main: "#9204d4", accent: "#bbe70e" },
-    { main: "#143078", accent: "#ef77eb" },
-    { main: "#0000ff", accent: "#b5d16f" },
-    { main: "#0000ff", accent: "#ecba32" },
-    { main: "#d5bbcb", accent: "#4b0032" },
-    { main: "#4b0082", accent: "#f9805a" },
-    { main: "#ff00ff", accent: "#001c5d" },
-    { main: "#ff00ff", accent: "#04121b" },
-    { main: "#ff0000", accent: "#05011a" },
-    { main: "#ff0000", accent: "#200c30" },
-    { main: "#ffd700", accent: "#1c3929" },
-    { main: "#ffd700", accent: "#031e04" },
-    { main: "#ffd700", accent: "#b10b6b" },
-    { main: "#ffd700", accent: "#256288" },
-    { main: "#b5d26f", accent: "#f44963" },
-    { main: "#a3977e", accent: "#f6f5ee" },
-    { main: "#2f2f2f", accent: "#fe6f27" },
-    { main: "#7ad9c4", accent: "#230071" },
-    { main: "#164058", accent: "#f5b51b" },
-    { main: "#f9848e", accent: "#f0355f" },
-    { main: "#514746", accent: "#edc8b2" },
-    { main: "#acdbe7", accent: "#7988de" },
-    { main: "#3c3c3c", accent: "#787878" },
+    { main: "hsl(360deg 100% 50%)", accent: "hsl(0deg 0% 0%)" },
+    { main: "hsl(0deg 0% 0%)", accent: "hsl(60deg 100% 50%)" },
+    { main: "hsl(120deg 100% 25%)", accent: "hsl(350deg 100% 88%)" },
+    { main: "hsl(0deg 0% 100%)", accent: "hsl(360deg 59% 41%)" },
+    { main: "hsl(208deg 100% 31%)", accent: "hsl(20deg 77% 72%)" },
+    { main: "hsl(71deg 96% 68%)", accent: "hsl(271deg 60% 51%)" },
+    { main: "hsl(351deg 89% 62%)", accent: "hsl(317deg 51% 7%)" },
+    { main: "hsl(72deg 58% 67%)", accent: "hsl(92deg 55% 13%)" },
+    { main: "hsl(330deg 100% 71%)", accent: "hsl(248deg 81% 31%)" },
+    { main: "#hsl(240deg 100% 50%)", accent: "#hsl(151deg 94% 51%)" },
+    { main: "hsl(16deg 100% 50%)", accent: "hsl(259deg 71% 20%)" },
+    { main: "hsl(16deg 100% 50%)", accent: "hsl(254deg 93% 5%)" },
+    { main: "hsl(16deg 100% 50%)", accent: "hsl(122deg 67% 7%)" },
+    { main: "hsl(0deg 0% 100%)", accent: "hsl(351deg 72% 52%)" },
+    { main: "hsl(0deg 0% 100%)", accent: "hsl(353deg 87% 19%)" },
+    { main: "hsl(0deg 0% 100%)", accent: "hsl(206deg 94% 41%)" },
+    { main: "hsl(0deg 0% 0%)", accent: "hsl(321deg 84% 68%)" },
+    { main: "hsl(0deg 0% 0%)", accent: "hsl(195deg 99% 52%)" },
+    { main: "hsl(0deg 0% 0%)", accent: "hsl(328deg 74% 52%)" },
+    { main: "hsl(0deg 0% 0%)", accent: "hsl(60deg 97% 54%)" },
+    { main: "hsl(0deg 0% 0%)", accent: "hsl(106deg 88% 38%)" },
+    { main: "hsl(0deg 0% 0%)", accent: "hsl(344deg 61% 65%)" },
+    { main: "hsl(94deg 4% 34%)", accent: "hsl(62deg 84% 52%)" },
+    { main: "hsl(199deg 100% 36%)", accent: "hsl(60deg 40% 2%)" },
+    { main: "hsl(281deg 96% 42%)", accent: "hsl(72deg 89% 48%)" },
+    { main: "hsl(223deg 71% 27%)", accent: "hsl(302deg 79% 70%)" },
+    { main: "hsl(240deg 100% 50%)", accent: "hsl(77deg 52% 63%)" },
+    { main: "hsl(240deg 100% 50%)", accent: "hsl(44deg 83% 56%)" },
+    { main: "hsl(323deg 24% 78%)", accent: "hsl(320deg 100% 15%)" },
+    {
+      main: "hsl(275deg 100% 25%)",
+      accent: "hsl(14deg 93% 66%)hsl(14deg 93% 66%)",
+    },
+    { main: "hsl(300deg 100% 50%)", accent: "hsl(222deg 100% 18%)" },
+    { main: "hsl(300deg 100% 50%)", accent: "hsl(203deg 74% 6%)" },
+    { main: "hsl(360deg 100% 50%)", accent: "hsl(250deg 93% 5%)" },
+    { main: "hsl(360deg 100% 50%)", accent: "hsl(273deg 60% 12%)" },
+    { main: "hsl(51deg 100% 50%)", accent: "hsl(147deg 34% 17%)" },
+    { main: "hsl(51deg 100% 50%)", accent: "hsl(122deg 82% 6%)" },
+    { main: "hsl(51deg 100% 50%)", accent: "hsl(325deg 88% 37%)" },
+    { main: "hsl(51deg 100% 50%)", accent: "hsl(203deg 57% 34%)" },
+    { main: "hsl(78deg 52% 63%)", accent: "hsl(351deg 89% 62%)" },
+    { main: "hsl(41deg 17% 57%)", accent: "hsl(53deg 31% 95%)" },
+    { main: "hsl(0deg 0% 18%)", accent: "hsl(20deg 99% 57%)" },
+    { main: "hsl(167deg 56% 66%)", accent: "hsl(259deg 100% 22%)" },
+    { main: "hsl(202deg 60% 22%)", accent: "hsl(42deg 92% 53%)" },
+    { main: "hsl(355deg 91% 75%)", accent: "hsl(347deg 86% 57%)" },
+    { main: "hsl(5deg 7% 30%)", accent: "hsl(22deg 62% 81%)" },
+    { main: "hsl(192deg 55% 79%)", accent: "hsl(231deg 60% 67%)" },
+    { main: "hsl(0deg 0% 24%)", accent: "hsl(0deg 0% 47%)" },
   ];
 
   // Choose a random color combination
