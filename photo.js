4464d5b8eaf9e6d37db15ad71cba218162876149
cko003 <cko003@bucknell.edu>
2024-03-01 09:05:58 +0100

    remove redundant css class selector "caption"

--- photo.js
+++ photo.js
@@ -49,32 +49,7 @@ function filterSelection(c) {
   }
   showSlides();
 }
-/*
-// Show filtered elements
-function AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
 
-// Hide elements that are not selected
-function RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-*/
 // Add active class to the current (photo sidebar) button (underline it)
 // const btnContainer = document.getElementById("side");
 const btns = document.getElementsByClassName("side-btn");
