3c4103fe273f81d32b99816f8a84b6cdb0759c45
cko003 <cko003@bucknell.edu>
2024-03-15 13:17:21 +0100

    add functionality to new cursor

--- js/cursor.js
+++ js/cursor.js
@@ -1,53 +1,55 @@
-let innerCursor = document.querySelector(".inner-cursor");
+// let innerCursor = document.querySelector(".inner-cursor");
 
-document.addEventListener("mousemove", moveCursor);
+// document.addEventListener("mousemove", moveCursor);
 
-function moveCursor(e) {
-  let x = e.clientX;
-  let y = e.clientY;
+// function moveCursor(e) {
+//   let x = e.clientX;
+//   let y = e.clientY;
 
-  innerCursor.style.left = `${x}px`;
-  innerCursor.style.top = `${y}px`;
-}
+//   innerCursor.style.left = `${x}px`;
+//   innerCursor.style.top = `${y}px`;
+// }
 
-//items that should grow the cursor
-let hoverables = Array.from(document.querySelectorAll("a"));
+// //items that should grow the cursor
+// let hoverables = Array.from(document.querySelectorAll("a"));
 
-hoverables = hoverables.concat(Array.from(document.querySelectorAll("button")));
+// hoverables = hoverables.concat(Array.from(document.querySelectorAll("button")));
 
-hoverables.forEach((hoverable) => {
-  hoverable.addEventListener("mouseover", () => {
-    innerCursor.classList.add("grow");
-  });
-  hoverable.addEventListener("mouseleave", () => {
-    innerCursor.classList.remove("grow");
-  });
-});
+// hoverables.forEach((hoverable) => {
+//   hoverable.addEventListener("mouseover", () => {
+//     innerCursor.classList.add("grow");
+//   });
+//   hoverable.addEventListener("mouseleave", () => {
+//     innerCursor.classList.remove("grow");
+//   });
+// });
 
-//items that should not grow the cursor but show text underneath it
-let differentables = Array.from(document.querySelectorAll("p"));
+// //items that should not grow the cursor but show text underneath it
+// let differentables = Array.from(document.querySelectorAll("p"));
 
-differentables = differentables.concat(
-  Array.from(document.querySelectorAll("form"))
-);
-differentables = differentables.concat(
-  Array.from(document.querySelectorAll(".header"))
-);
-differentables = differentables.concat(
-  Array.from(document.querySelectorAll(".grid"))
-);
+// differentables = differentables.concat(
+//   Array.from(document.querySelectorAll("form"))
+// );
+// differentables = differentables.concat(
+//   Array.from(document.querySelectorAll(".header"))
+// );
+// differentables = differentables.concat(
+//   Array.from(document.querySelectorAll(".grid"))
+// );
 
-differentables.forEach((differentable) => {
-  differentable.addEventListener("mouseover", () => {
-    innerCursor.classList.add("different");
-  });
-  differentable.addEventListener("mouseleave", () => {
-    innerCursor.classList.remove("different");
-  });
-});
+// differentables.forEach((differentable) => {
+//   differentable.addEventListener("mouseover", () => {
+//     innerCursor.classList.add("different");
+//   });
+//   differentable.addEventListener("mouseleave", () => {
+//     innerCursor.classList.remove("different");
+//   });
+// });
 
-// let cursor = document.querySelector("div.cursors");
-let balls = document.querySelectorAll("div.cursors div");
+let cursorTag = document.querySelector("div.cursors");
+let balls = cursorTag.querySelectorAll("div");
+let ballMessage = cursorTag.querySelector("div span");
+const images = document.querySelectorAll("img[data-hover]");
 
 let aimX = 0;
 let aimY = 0;
@@ -75,4 +77,15 @@ balls.forEach((ball, index) => {
 document.addEventListener("mousemove", function (e) {
   aimX = e.pageX;
   aimY = e.pageY;
+});
+
+images.forEach((image) => {
+  image.addEventListener("mouseover", () => {
+    ballMessage.classList.add("visible");
+    ballMessage.innerHTML = image.getAttribute("data-hover");
+  });
+  image.addEventListener("mouseout", () => {
+    ballMessage.classList.remove("visible");
+    ballMessage.innerHTML = "";
+  });
 });
