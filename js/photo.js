4e221526e46b8f1e87e7b656827020d7503088f3
cko003 <cko003@bucknell.edu>
2024-03-16 20:16:23 +0100

    image grid

--- js/photo.js
+++ js/photo.js
@@ -1,138 +1,138 @@
-let slideIndex = 0;
-let slides = document.getElementsByClassName("slide");
-const TOTAL_SLIDE_LENGTH = slides.length;
-filterSelection("slide"); // Begin by showing all
-plusSlides(0);
+// let slideIndex = 0;
+// let slides = document.getElementsByClassName("slide");
+// const TOTAL_SLIDE_LENGTH = slides.length;
+// filterSelection("slide"); // Begin by showing all
+// plusSlides(0);
 
-// Next/previous controls
-function plusSlides(n) {
-  slideIndex = (slideIndex + n + slides.length) % slides.length;
-  while (slides[slideIndex].classList.contains("hidden")) {
-    slideIndex = (slideIndex + n + slides.length) % slides.length;
-  }
-  showSlides();
+// // Next/previous controls
+// function plusSlides(n) {
+//   slideIndex = (slideIndex + n + slides.length) % slides.length;
+//   while (slides[slideIndex].classList.contains("hidden")) {
+//     slideIndex = (slideIndex + n + slides.length) % slides.length;
+//   }
+//   showSlides();
 
-  // Load the next image when you go to the next slide
-  const nextIndex = (slideIndex + 1) % slides.length;
-  let prevIndex;
-  if (slideIndex === 0) {
-    prevIndex = slides.length - 1;
-  } else {
-    prevIndex = (slideIndex - 1) % slides.length;
-  }
-  const nextImage = slides[nextIndex].querySelector("img");
-  const prevImage = slides[prevIndex].querySelector("img");
-  if (nextImage.loading === "lazy" || prevImage.loading === "lazy") {
-    // Change loading behavior to eager
-    nextImage.loading = "eager";
-    prevImage.loading = "eager";
-  }
-}
+//   // Load the next image when you go to the next slide
+//   const nextIndex = (slideIndex + 1) % slides.length;
+//   let prevIndex;
+//   if (slideIndex === 0) {
+//     prevIndex = slides.length - 1;
+//   } else {
+//     prevIndex = (slideIndex - 1) % slides.length;
+//   }
+//   const nextImage = slides[nextIndex].querySelector("img");
+//   const prevImage = slides[prevIndex].querySelector("img");
+//   if (nextImage.loading === "lazy" || prevImage.loading === "lazy") {
+//     // Change loading behavior to eager
+//     nextImage.loading = "eager";
+//     prevImage.loading = "eager";
+//   }
+// }
 
-document.addEventListener("keydown", function (e) {
-  var keyPressed = e.key;
-  if (keyPressed === "ArrowLeft") {
-    //Left Key Press
-    // console.log("left");
-    plusSlides(-1);
-  } else if (keyPressed === "ArrowRight") {
-    // Right Key Press
-    // console.log("right");
-    plusSlides(1);
-  }
-});
+// document.addEventListener("keydown", function (e) {
+//   var keyPressed = e.key;
+//   if (keyPressed === "ArrowLeft") {
+//     //Left Key Press
+//     // console.log("left");
+//     plusSlides(-1);
+//   } else if (keyPressed === "ArrowRight") {
+//     // Right Key Press
+//     // console.log("right");
+//     plusSlides(1);
+//   }
+// });
 
-// Sets a display of none to every slide except the current one
-function showSlides() {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex].style.display = "block";
-  let currentSlideNumber = slides[slideIndex].querySelector(
-    ":scope > .caption > .current-slide"
-  );
-  // pad strings with 0s until it equals slide length
-  currentSlideNumber.textContent = `${String(slideIndex + 1).padStart(
-    String(TOTAL_SLIDE_LENGTH).length,
-    "0"
-  )}/${String(slides.length).padStart(String(TOTAL_SLIDE_LENGTH).length, "0")}`;
-}
+// // Sets a display of none to every slide except the current one
+// function showSlides() {
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex].style.display = "block";
+//   let currentSlideNumber = slides[slideIndex].querySelector(
+//     ":scope > .caption > .current-slide"
+//   );
+//   // pad strings with 0s until it equals slide length
+//   currentSlideNumber.textContent = `${String(slideIndex + 1).padStart(
+//     String(TOTAL_SLIDE_LENGTH).length,
+//     "0"
+//   )}/${String(slides.length).padStart(String(TOTAL_SLIDE_LENGTH).length, "0")}`;
+// }
 
-function filterSelection(c) {
-  // slides = document.getElementsByClassName("slide");
-  // if (c == "all") c = "";
-  // hide elemetns that are not active
-  // slides[slideIndex].classList.add("hidden");
-  for (let i = 0; i < slides.length; i++) {
-    console.log(slides.length);
-    if (!slides[i].classList.contains(c)) {
-      slides[i].classList.add("hidden");
-      slides[i].style.display = "none";
-    }
-    // AddClass(slides[i], "hidden");
-  }
-  slides = document.getElementsByClassName(c);
-  slideIndex = 0;
+// function filterSelection(c) {
+//   // slides = document.getElementsByClassName("slide");
+//   // if (c == "all") c = "";
+//   // hide elemetns that are not active
+//   // slides[slideIndex].classList.add("hidden");
+//   for (let i = 0; i < slides.length; i++) {
+//     console.log(slides.length);
+//     if (!slides[i].classList.contains(c)) {
+//       slides[i].classList.add("hidden");
+//       slides[i].style.display = "none";
+//     }
+//     // AddClass(slides[i], "hidden");
+//   }
+//   slides = document.getElementsByClassName(c);
+//   slideIndex = 0;
 
-  for (let i = 0; i < slides.length; i++) {
-    if (slides[i].classList.contains(c)) {
-      // RemoveClass(slides[i], "hidden");
-      slides[i].classList.remove("hidden");
-      // slides[i].style.display = "block";
-    }
-  }
-  if (slides[slideIndex].classList.contains("hidden")) {
-    plusSlides(1);
-  }
+//   for (let i = 0; i < slides.length; i++) {
+//     if (slides[i].classList.contains(c)) {
+//       // RemoveClass(slides[i], "hidden");
+//       slides[i].classList.remove("hidden");
+//       // slides[i].style.display = "block";
+//     }
+//   }
+//   if (slides[slideIndex].classList.contains("hidden")) {
+//     plusSlides(1);
+//   }
 
-  showSlides();
-}
+//   showSlides();
+// }
 
-// Add active class to the current (photo sidebar) button (underline it)
-// const btnContainer = document.getElementById("side");
-const btns = document.getElementsByClassName("side-btn");
-// Loop through all buttons and add click event listeners
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    // Remove "active" class from all buttons
-    for (var j = 0; j < btns.length; j++) {
-      btns[j].classList.remove("side-active");
-    }
-    // Add "active" class to the clicked button
-    this.classList.add("side-active");
-  });
-}
+// // Add active class to the current (photo sidebar) button (underline it)
+// // const btnContainer = document.getElementById("side");
+// const btns = document.getElementsByClassName("side-btn");
+// // Loop through all buttons and add click event listeners
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     // Remove "active" class from all buttons
+//     for (var j = 0; j < btns.length; j++) {
+//       btns[j].classList.remove("side-active");
+//     }
+//     // Add "active" class to the clicked button
+//     this.classList.add("side-active");
+//   });
+// }
 
-// Get the elements with class="column"
-const elements = document.getElementsByClassName("column");
+// // Get the elements with class="column"
+// const elements = document.getElementsByClassName("column");
 
-// Declare a loop variable
+// // Declare a loop variable
 
-// Full-width images
-function one() {
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "100%";
-  }
-  // showSlides(slideIndex);
-}
+// // Full-width images
+// function one() {
+//   for (let i = 0; i < elements.length; i++) {
+//     elements[i].style.flex = "100%";
+//   }
+//   // showSlides(slideIndex);
+// }
 
-// Four images side by side
-function four() {
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "25%";
-  }
-  // showSlides(slideIndex);
-}
+// // Four images side by side
+// function four() {
+//   for (let i = 0; i < elements.length; i++) {
+//     elements[i].style.flex = "25%";
+//   }
+//   // showSlides(slideIndex);
+// }
 
-// Add active class to the current (grid) button (highlight it)
-// var header = document.getElementById("myHeader");
-const photobtns = document.getElementsByClassName("photobtn");
-for (var i = 0; i < photobtns.length; i++) {
-  photobtns[i].addEventListener("click", function () {
-    for (var j = 0; j < photobtns.length; j++) {
-      photobtns[j].classList.remove("grid-active");
-    }
-    // Add "active" class to the clicked button
-    this.classList.add("grid-active");
-  });
-}
+// // Add active class to the current (grid) button (highlight it)
+// // var header = document.getElementById("myHeader");
+// const photobtns = document.getElementsByClassName("photobtn");
+// for (var i = 0; i < photobtns.length; i++) {
+//   photobtns[i].addEventListener("click", function () {
+//     for (var j = 0; j < photobtns.length; j++) {
+//       photobtns[j].classList.remove("grid-active");
+//     }
+//     // Add "active" class to the clicked button
+//     this.classList.add("grid-active");
+//   });
+// }
